a8f0ea92c86bd08cf9ba055ceb77781fdbcd00bf
Manuela Souza <manuela.souza@fulltime.com.br>
2024-11-08 17:11:22 -0300

    primeiro commit

---
+++ app/src/main/java/br/com/alura/ceep/database/dao/NotaDao.kt
@@ -0,0 +1,25 @@
+package br.com.alura.ceep.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.alura.ceep.model.Nota
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NotaDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun salva(note: Nota)
+
+    @Query("SELECT * FROM Nota")
+    fun buscaTodas() : Flow<List<Nota>>
+
+    @Query("SELECT * FROM Nota WHERE id = :id")
+    fun buscaPorId(id: Long): Flow<Nota>
+
+    @Query("DELETE FROM Nota WHERE id = :id")
+    suspend fun remove(id: Long)
+
+}
